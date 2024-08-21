f7f9c5cd711c6fb64de9eff6d28988565afe60e4
mpx86 <28656499+mpx86@users.noreply.github.com>
2024-08-21 13:02:05 -0400

    Added vars and system updates to Dockerfile

--- Docker/entrypoint.sh
+++ Docker/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Exit immediately if a command exits with a non-zero status
-set -e
+#set -e
 
 # Activate the virtual environment
 source /app/venv/bin/activate
@@ -13,24 +13,24 @@ if [ ! -f "$SERVER_DIR/.serverversion" ]; then
 fi
 
 # Start devpi-server in the background to allow creating users and indices
-devpi-server --serverdir "$SERVER_DIR" \
+exec devpi-server --serverdir "$SERVER_DIR" \
     --host 0.0.0.0 \
-    --port "$DEVPI_PORT" &
+    --port "$DEVPI_PORT" #&
 
 # Wait for the server to start
-sleep 5
+# sleep 5
 
 # Checking /app/devpi contents
-echo "Checking /app/devpi contents"
-ls -al /app/devpi/
+# echo "Checking /app/devpi contents"
+# ls -al /app/devpi/
 
-# Checking /app/venv contents
-echo "Checking /app/venv contents"
-ls -al /app/venv/
+# # Checking /app/venv contents
+# echo "Checking /app/venv contents"
+# ls -al /app/venv/
 
-# Set up the devpi user and the 'packages' index
-echo "Attempting to use http://localhost:$DEVPI_PORT"
-devpi use http://localhost:$DEVPI_PORT
+# # Set up the devpi user and the 'packages' index
+# echo "Attempting to use http://localhost:$DEVPI_PORT"
+# devpi use http://localhost:$DEVPI_PORT
 
 # echo "Attempting to create $DEVPI_INTERNAL_USER"
 # devpi user -c $DEVPI_INTERNAL_USER password="$DEVPI_PWHASH"
